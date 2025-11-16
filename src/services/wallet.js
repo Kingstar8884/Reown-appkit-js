3710bfa4b5a72284e7ff1d2f835debd0f20aa75e
Kingstar <fasasiayomide03@gmail.com>
2025-11-16 03:58:15 +0100

    N/A

---
+++ src/services/wallet.js
@@ -0,0 +1,41 @@
+import { BrowserProvider, Contract, formatUnits, parseEther } from 'ethers'
+
+export const signMessage = (provider, address) => {
+    if (!provider) return Promise.reject('No provider available')
+    
+    return provider.request({
+      method: 'personal_sign',
+      params: ['Hello from AppKit!', address]
+    })
+  }
+
+  export const sendTx = async (provider, address) => {
+    if (!provider) return Promise.reject('No provider available')
+
+      const tx = {
+        from: address,
+        to: "0x302D8DA8967f9afA00f1DcdbD70aF0F30784BDF2", // same address just for testing
+        value: '0x' + parseEther("0.00001").toString(16)
+      }
+
+      const test = await provider.request({
+        method: "eth_sendTransaction",
+        params: [tx]
+      });
+
+      console.log(test);
+      return test;
+      const ethersProvider = new BrowserProvider(provider);
+      const signer = await ethersProvider.getSigner()
+      return await signer.sendTransaction(tx)
+  }
+
+  export const getBalance = async (provider, address) => {
+    if (!provider) return Promise.reject('No provider available')
+    
+    const balance = await provider.request({
+      method: 'eth_getBalance',
+      params: [address, 'latest']
+    })
+    return formatUnits(balance, 'ether')
+  }
