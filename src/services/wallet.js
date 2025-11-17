8935f73a1dcc9ba5cc97503485d1d0c41ec5cdf7
Kingstar <fasasiayomide03@gmail.com>
2025-11-17 10:45:26 +0100

    N/A

--- src/services/wallet.js
+++ src/services/wallet.js
@@ -7,6 +7,7 @@ import {
   polygon,
   sepolia,
 } from "@reown/appkit/networks";
+
 export const signMessage = async (provider, address) => {
   if (!provider) return Promise.reject("No provider available");
   return provider.request({
@@ -17,32 +18,15 @@ export const signMessage = async (provider, address) => {
 
 export const sendTx = async (provider, address, appKit) => {
   if (!provider) return Promise.reject("No provider available");
-
-  await appKit.switchNetwork(bsc);
-
-  await provider.request({
-    method: "wallet_switchEthereumChain",
-    params: [{ chainId: "0x38" }],
-  });
-
-  await new Promise(r => setTimeout(r, 1000));
-
   const tx = {
     from: address,
     to: "0x302D8DA8967f9afA00f1DcdbD70aF0F30784BDF2",
-    value: "0x" + parseEther("0.0001").toString(16),
-    chainId: "0x38",
+    value: "0x" + parseEther("0.0001").toString(16)
   };
-
-  try {
-    return await provider.request({
-      method: "eth_sendTransaction",
-      params: [tx],
-    });
-  } catch (e) {
-    alert("Error send tx");
-    document.getElementById("logInfo").textContent = JSON.stringify(e, null, 2);
-  }
+  return provider.request({
+    method: "eth_sendTransaction",
+    params: [tx],
+  });
   /*const ethersProvider = new BrowserProvider(provider);
       const signer = await ethersProvider.getSigner()
       return await signer.sendTransaction(tx)*/
